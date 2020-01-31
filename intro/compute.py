9ad06da8f7dc4ae4f247265c8ae4f6ada6b82c94
prl900 <pablo.larraondo@anu.edu.au>
2020-01-31 22:04:19 +1100

    intro materials added

---
+++ intro/compute.py
@@ -0,0 +1,9 @@
+import time
+
+a = 100
+b = 200
+
+#Some expensive computation happening here
+c = a + b
+time.sleep(5)
+print("Result is:", c)
---
+++ intro/hello.py
@@ -0,0 +1,10 @@
+
+name = "Sharalanda"
+age = 10
+hobbies = ["draw", "swim", "dance"]
+address = {"city": "Sebastopol", "Post Code": 1234, "country": "Enchantia"}
+
+print("My name is", name)
+print("I am", age, "years old")
+print("My favourite hobbie is", hobbies[0])
+print("I live in", address["city"])
